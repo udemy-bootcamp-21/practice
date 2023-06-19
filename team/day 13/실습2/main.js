44f8dcd543de03a1aecd1737c1acdd1dac5a9cb0
Eun-T <venzire329@gmail.com>
2023-06-19 15:39:49 +0900

    Feat: '완성'

--- team/day 13/실습2/main.js
+++ team/day 13/실습2/main.js
@@ -4,24 +4,16 @@ const div__item = document.createElement("div")
 const h2 = document.createElement("h2")
 const p = document.createElement("p")
 
-// postContainer.innerHTML += `
-//   <div class="post__container">
-//     <div class="post__item">
-//       <h2>첫 번째 게시글</h2>
-//       <p>안녕하세요!</p>
-//     </div>
-//     <div class="post__item">
-//       <h2>두 번째 게시글</h2>
-//       <p>안녕하세요!</p>
-//     </div>
-//   </div>
-// `
+postContainer.innerHTML += `
+  <div class="post__container">
+    <div class="post__item">
+      <h2>첫 번째 게시글</h2>
+      <p>안녕하세요!</p>
+    </div>
+    <div class="post__item">
+      <h2>두 번째 게시글</h2>
+      <p>안녕하세요!</p>
+    </div>
+  </div>
+`
 
-h2.textContent = "첫 번째 게시글"
-p.textContent = "안녕하세요!"
-
-div__item.appendChild(h2)
-div__item.appendChild(p)
-div__container.append(div__item)
-postContainer.appendChild(div__container)
-postContainer.appendChild(div__container)
