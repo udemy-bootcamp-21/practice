db09c53e30c33cf149423e5233a213a5a2f95ac9
jg <jgkang9402@gmail.com>
2023-07-03 16:10:26 +0900

    로그인버튼리덕스 실습

--- jeonggu/reduxApp/src/App.jsx
+++ jeonggu/reduxApp/src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment } from "./store/counterSlice";
+import { handleLogin } from "./store/authSlice";
 
 function App() {
   const count = useSelector((state) => state.counter.value);
+  const isLogin = useSelector((state) => state.auth.isLogin);
+  const notiTxt = useSelector((state) => state.auth.notiTxt);
+  const btnTxt = useSelector((state) => state.auth.btnTxt);
   const dispatch = useDispatch();
 
   return (
@@ -11,6 +15,10 @@ function App() {
       <div>{count}</div>
       <button onClick={() => dispatch(increment())}>증가</button>
       <button onClick={() => dispatch(decrement())}>감소</button>
+      <div>
+        <p>{notiTxt}</p>
+        <button onClick={() => dispatch(handleLogin(isLogin))}>{btnTxt}</button>
+      </div>
     </>
   );
 }
---
+++ jeonggu/reduxApp/src/store/authSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLogin: false,
+  notiTxt: "로그인되지 않았습니다.",
+  btnTxt: "로그인",
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    handleLogin: (state, action) => {
+      state.isLogin = !action.payload;
+      if (state.isLogin) {
+        state.notiTxt = "로그인 되었습니다.";
+        state.btnTxt = "로그아웃";
+      } else {
+        state.notiTxt = "로그인되지 않았습니다.";
+        state.btnTxt = "로그인";
+      }
+    },
+  },
+});
+
+export const { handleLogin } = authSlice.actions;
+// export const { increment, decrement, incrementByAmount } = authSlice.actions;
+
+export default authSlice.reducer;
